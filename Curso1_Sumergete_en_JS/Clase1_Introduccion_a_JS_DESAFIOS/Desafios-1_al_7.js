731c6462b81db996ce4a4159d070c086decd1fb0
Lorl <grassiantonella747@gmail.com>
2025-01-16 15:42:50 -0300

    add

---
+++ Curso1_Sumergete_en_JS/Clase1_Introduccion_a_JS_DESAFIOS/Desafios-1_al_7.js
@@ -0,0 +1,29 @@
+// 1.Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
+// 2.Declara una variable llamada nombre y asígnale el valor "Lua".
+// 3.Crea una variable llamada edady asígnale el valor 25.
+// 4.Establece una variable numeroDeVentas y asígnale el valor 50.
+// 5.Establece una variable saldoDisponible y asígnale el valor 1000.
+// 6.Muestra una alerta con el texto "¡Error! Completa todos los campos".
+// 7.Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
+
+//1
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+
+//2
+let nombre="Lua";
+
+//3
+let edad=25;
+
+//4
+let numeroDeVentas=50;
+
+//5
+let saldoDisponible=1000;
+
+//6
+alert("¡Error! Completa todos los campos");
+
+//7
+let mensajeDeError="¡Error! Completa todos los campos";
+alert(mensajeDeError);
---
+++ Curso1_Sumergete_en_JS/Clase1_Introduccion_a_JS_DESAFIOS/Desafios-8_al_10.js
@@ -0,0 +1,10 @@
+// 8.Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
+// 9.Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+// 10.Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".
+
+let nombre=prompt("Ingrese su nombre:");
+let edad=prompt("Ingrese su edad:");
+
+if(edad>=18){
+    alert("¡Puedes obtener tu licencia de conducir!");
+}
