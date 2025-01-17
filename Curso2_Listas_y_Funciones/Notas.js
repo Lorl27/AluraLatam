13a9bed29d2404582b32d423869f04716fa3539c
Lorl <grassiantonella747@gmail.com>
2025-01-17 16:08:50 -0300

    Clase 1

---
+++ Curso2_Listas_y_Funciones/Notas.js
@@ -0,0 +1,21 @@
+let titulo= document.querySelector("h1");
+titulo.innerHTML= "This is a tittle";
+
+/* HTML :
+<meta code>
+<head>
+<script src="file.js"> </script>
+</head> 
+
+<body>
+
+<h1> </h1>
+<p class="parrafo"> </p>
+
+<button class="button_syle" onclick="inicar_game();"> Iniciar juego </button>
+</body>
+*/
+
+function inicar_game(){
+    //do something
+}
